f96aaa2a52ae6cff5e9be2d894168a520e0b7bee
Ted Senft <code@robotgryphon.com>
2018-02-11 14:57:08 -0500

    Add livereload functionality and a debug task for Code.

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const path = require("path");
+const livereload = require('livereload');
 
 const httpPort = 3031;
-
-
-var app = express();
-
 const staticPath = path.join(__dirname, "public");
 
-app.use(express.static(staticPath));
-app.use(express.static(path.join(staticPath, "template")));
+startServer();
 
-app.listen(httpPort);
+console.log("Creating livereload server.");
+var lreload = livereload.createServer();
+lreload.watch(staticPath);
+
+console.log(`Started at http://localhost:${httpPort}`);
+
+
+
+function startServer() {
+    console.log("Creating express server.");
+    var app = express();
+
+    console.log(`Hosting from ${staticPath}...`);
+    app.use(express.static(staticPath));
+    
+    
+    // Start up express server
+    console.log(`Starting on port ${httpPort}...`);
+    app.listen(httpPort);
+}
